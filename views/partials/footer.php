3c3c378e195ef33b107725724ba336aa1d9d6763
odeandialamsyah <odeandialamsyah@gmail.com>
2024-07-21 05:11:53 +0700

    footer

---
+++ views/partials/footer.php
@@ -0,0 +1,7 @@
+<!-- footer.php -->
+</main>
+    <footer class="bg-gray-800 text-white p-4 text-center">
+        <p>&copy; 2024 Toko Online. All rights reserved.</p>
+    </footer>
+</body>
+</html>
