be42c341a6fab4d12ed936b8613dd02ec76bff4e
odeandialamsyah <odeandialamsyah@gmail.com>
2024-07-21 05:25:05 +0700

    header

--- views/partials/header.php
+++ views/partials/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="public/css/tailwind.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <title>Toko Online</title>
 </head>
 <body class="bg-gray-100">
